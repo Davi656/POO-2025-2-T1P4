e544d39ef9edbf1dc3804b10ddc62c1dd2722f56
Davi Mendes <davimendessmedeiros@gmail.com>
2025-09-30 11:17:04 -0300

    Update Principal.java

--- Lista-01/Exercícios-Propostos/Q02/src/br/edu/principal/Principal.java
+++ Lista-01/Exercícios-Propostos/Q02/src/br/edu/principal/Principal.java
@@ -4,22 +4,19 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-		
-        Scanner sc = new Scanner(System.in);
-        
-        double n1, n2, n3, multiplicacao;
- 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Digite o primeiro número: ");
-        n1 = sc.nextDouble();
+        double numero1 = scanner.nextDouble();
 
         System.out.print("Digite o segundo número: ");
-        n2 = sc.nextDouble();
+        double numero2 = scanner.nextDouble();
 
         System.out.print("Digite o terceiro número: ");
-        n3 = sc.nextDouble();
+        double numero3 = scanner.nextDouble();
 
-        multiplicacao = n1 * n2 * n3;
-      
-        System.out.println("O resultado da multiplicação é: " + multiplicacao);
+        double produto = numero1 * numero2 * numero3;
+
+        System.out.println("O resultado da multiplicação é: " + produto);
     }
 }
