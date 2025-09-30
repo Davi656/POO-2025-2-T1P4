e2125f90660ceba8a7af5613f6e129746e7ecd7d
Davi Mendes <davimendessmedeiros@gmail.com>
2025-09-30 12:40:13 -0300

    Update Principal.java

--- Lista-03/Exercícios-Propostos/Q03/src/br/edu/principal/Principal.java
+++ Lista-03/Exercícios-Propostos/Q03/src/br/edu/principal/Principal.java
@@ -8,12 +8,13 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite o primeiro número: ");
-        int num1 = sc.nextInt();
+    int num1 = sc.nextInt();
 
         System.out.print("Digite o segundo número: ");
-        int num2 = sc.nextInt();
+    int num2 = sc.nextInt();
 
-        int resultado = calculo.verificarDivisivel(num1, num2);
+    int resultado = calculo.verificarDivisivel(num1, num2);
         System.out.println("Resultado: " + resultado);
     }
 }
+
