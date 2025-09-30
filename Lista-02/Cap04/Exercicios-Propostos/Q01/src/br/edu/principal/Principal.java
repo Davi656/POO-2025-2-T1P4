8cde7c53584a337607a595c3cb33cae89809b9d6
Davi Mendes <davimendessmedeiros@gmail.com>
2025-09-30 12:15:41 -0300

    Update Principal.java

--- Lista-02/Cap04/Exercicios-Propostos/Q01/src/br/edu/principal/Principal.java
+++ Lista-02/Cap04/Exercicios-Propostos/Q01/src/br/edu/principal/Principal.java
@@ -1,34 +1,33 @@
-package br.edu.principal;
-
+package Exerc.Propostos;
 import java.util.Scanner;
 
-public class Principal {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		double n1 , n2 , n3 , n4 , media;
-		
-		System.out.println("Digite a primeira nota: ");
-		n1 = sc.nextDouble();
-		
-		System.out.println("Digite a segunda nota: ");
-		n2 = sc.nextDouble();
-		
-		System.out.println("Digite a terceira nota: ");
-		n3 = sc.nextDouble();
-		
-		System.out.println("Digite a quarta nota: ");
-		n4 = sc.nextDouble();
-		
-		media = (n1+n2+n3+n4)/4;
-		
-		if (media >= 7) {
-			System.out.println("Aluno aprovado com média: " + media);
-		} else {
-			System.out.println("Aluno reprovado com média: " + media);
-		}
-		
-	}
+public class Q01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá! Vamos calcular a média de um aluno.");
+        System.out.print("Digite a primeira nota: ");
+        double n1 = sc.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double n2 = sc.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        double n3 = sc.nextDouble();
+
+        System.out.print("Digite a quarta nota: ");
+        double n4 = sc.nextDouble();
+
+        double media = (n1 + n2 + n3 + n4) / 4;
+        System.out.println("A média do aluno é: " + media);
+
+        if (media >= 7) {
+            System.out.println("Parabéns! O aluno foi aprovado!");
+        } else {
+            System.out.println("Infelizmente, o aluno foi reprovado.");
+        }
+
+        sc.close();
+    }
 }
 
