0852938cb9998978004803fc0e98d9b9d13fefb5
Davi Mendes <davimendessmedeiros@gmail.com>
2025-09-30 12:20:24 -0300

    Update Principal.java

--- Lista-02/Cap04/Exercicios-Propostos/Q02/src/br/edu/principal/Principal.java
+++ Lista-02/Cap04/Exercicios-Propostos/Q02/src/br/edu/principal/Principal.java
@@ -7,25 +7,26 @@ public class Q02 {
 
         System.out.println("Vamos calcular a média de um aluno e informar a situação.");
         System.out.print("Digite a primeira nota: ");
-        double n1 = sc.nextDouble();
+     double n1 = sc.nextDouble();
 
         System.out.print("Digite a segunda nota: ");
-        double n2 = sc.nextDouble();
+     double n2 = sc.nextDouble();
 
-        double media = (n1 + n2) / 2;
+     double media = (n1 + n2) / 2;
         System.out.println("A média do aluno é: " + media);
 
         if (media < 3) {
             System.out.println("O aluno está reprovado.");
         } else if (media < 7) {
             System.out.println("O aluno ficou para exame.");
-            double notaExame = 6 - media;
+     double notaExame = 6 - media;
             System.out.println("Para ser aprovado, precisa tirar pelo menos: " + notaExame);
         } else {
             System.out.println("Parabéns! O aluno foi aprovado!");
         }
 
-        sc.close();
+    sc.close();
     }
 }
+
 
