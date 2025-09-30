17bb6f9fb27815e8c79e681a399addd329ecb783
Davi Mendes <davimendessmedeiros@gmail.com>
2025-09-30 12:39:43 -0300

    Update Principal.java

--- Lista-03/Exercícios-Propostos/Q01/src/br/edu/principal/Principal.java
+++ Lista-03/Exercícios-Propostos/Q01/src/br/edu/principal/Principal.java
@@ -7,10 +7,11 @@ public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite um número inteiro positivo N: ");
-        int n = sc.nextInt();
+    int n = sc.nextInt();
 
-        int soma = Calculo.somaNumerosEntre1eN(n);
+    int soma = Calculo.somaNumerosEntre1eN(n);
         System.out.println("A soma dos números entre 1 e " + n + " é: " + soma);
     }
+
 
 }
