087a45d61255845c148192214e319dc1d9823a26
Davi Mendes <davimendessmedeiros@gmail.com>
2025-09-30 12:23:42 -0300

    Update Principal.java

--- Lista-02/Cap04/Exercicios-Resolvidos/Q02/src/br/edu/principal/Principal.java
+++ Lista-02/Cap04/Exercicios-Resolvidos/Q02/src/br/edu/principal/Principal.java
@@ -7,38 +7,40 @@ public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nota1, nota2, nota3, media, nota_exame;
+        double nota1 = lerNota(sc, "primeira");
+        double nota2 = lerNota(sc, "segunda");
+        double nota3 = lerNota(sc, "terceira");
 
-        System.out.println("Digite a primeira nota:");
-        nota1 = sc.nextDouble();
+        double media = calcularMedia(nota1, nota2, nota3);
 
-        System.out.println("Digite a segunda nota:");
-        nota2 = sc.nextDouble();
-
-        System.out.println("Digite a terceira nota:");
-        nota3 = sc.nextDouble();
-
-        media = (nota1 + nota2 + nota3) / 3;
-        System.out.println("Média aritmética: " + media);
+        System.out.printf("Média aritmética: %.2f%n", media);
 
         if (media >= 0 && media < 3) {
             System.out.println("Reprovado");
-        } 
-        
-        else if (media >= 3 && media < 7) {
+        } else if (media >= 3 && media < 7) {
             System.out.println("Exame");
-            nota_exame = 12 - media;
-            System.out.println("Deve tirar nota " + nota_exame + " para ser aprovado");
-        } 
-        
-        else if (media >= 7 && media <= 10) {
+            double notaExame = 12 - media;
+            System.out.printf("Deve tirar pelo menos %.2f no exame para ser aprovado%n", notaExame);
+        } else if (media >= 7 && media <= 10) {
             System.out.println("Aprovado");
-        } 
-        
-        else {
-            System.out.println("Nota inválida");
+        } else {
+            System.out.println("Média inválida");
         }
 
         sc.close();
     }
-}
+
+    public static double lerNota(Scanner sc, String ordem) {
+        System.out.printf("Digite a %s nota: ", ordem);
+        double nota = sc.nextDouble();
+
+        while (nota < 0 || nota > 10) {
+            System.out.println("Nota inválida. Digite novamente (entre 0 e 10): ");
+            nota = sc.nextDouble();
+        }
+
+        return nota;
+    }
+
+    public static double calcularMedia(double n1, double n2, double n3) {
+        return (n1 + n2 + n3) / 3.0
